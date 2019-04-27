06dc4ef1272e7601a6baadcd341b79f549fd0b64
marcinjak9 <marcinjak9@gmail.com>
2019-04-27 15:36:32 +0200

    added manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     description:
       'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
     author: '@gatsbyjs',
+    siteUrl: 'https://apical.org',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -37,15 +38,8 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
-          // {
-          //   resolve: 'gatsby-remark-prismjs',
-          //   options: {},
-          // },
           {
             resolve: 'gatsby-remark-relative-images',
-            // options: {
-            //   name: 'static/images',
-            // },
           },
           {
             resolve: 'gatsby-remark-images',
@@ -57,12 +51,54 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Apical - Become an Experience Creator',
+        short_name: 'Apical',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#252D3B',
+        theme_color: '#252D3B',
         display: 'minimal-ui',
+        icons: [
+          {
+            src: 'src/images/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+          },
+          {
+            src: 'src/images/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: 'src/images/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+          },
+          {
+            src: 'src/images/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: 'src/images/icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+          },
+          {
+            src: 'src/images/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'src/images/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: 'src/images/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
         // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
@@ -73,9 +109,10 @@ module.exports = {
         name: 'markdown-pages',
       },
     },
+    'gatsby-plugin-sitemap',
     // 'gatsby-transformer-remark',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    'gatsby-plugin-offline',
   ],
 }
