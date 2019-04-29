d950c87569074376f95c9c396ee298f3e401d825
marcinjak9 <marcinjak9@gmail.com>
2019-04-29 10:04:09 +0200

    Uploadcare custom package

--- src/components/image.js
+++ src/components/image.js
@@ -1,67 +1,14 @@
 import React from 'react'
-import Img from 'gatsby-image'
+import UploadCareImage, { getImageUrl } from 'uploadcare-react'
 
-const Image = ({
-  image, className, style, alt = '',
-}) => {
-  if (!image) {
-    return null
-  }
-  if (image.childImageSharp) {
-    const { childImageSharp } = image
-    if (childImageSharp.fluid) {
-      return (
-        <Img
-          fluid={childImageSharp.fluid}
-          alt={alt}
-          className={className}
-          style={style}
-        />
-      )
-    }
-    if (childImageSharp.fixed) {
-      return (
-        <Img
-          fixed={childImageSharp.fixed}
-          alt={alt}
-          className={className}
-          style={style}
-        />
-      )
-    }
-    return null
-  }
-  if (typeof image === 'string') {
-    return <img src={image} className={className} style={style} alt={alt} />
-  }
-  if (image.value) {
-    return (
-      <img src={image.value} className={className} style={style} alt={alt} />
-    )
-  }
-  return null
+const Image = (props) => {
+  const { src } = props
+  return <UploadCareImage src={src} {...props} preview />
 }
 
-export const getImageLink = (image) => {
-  if (!image) {
-    return ''
-  }
-  if (typeof image === 'string') {
-    return image
-  }
-  if (image.childImageSharp) {
-    const { childImageSharp } = image
-    if (childImageSharp.fluid) {
-      return childImageSharp.fluid.src
-    }
-    if (childImageSharp.fixed) {
-      return childImageSharp.fixed.src
-    }
-  }
-  if (image.value) {
-    return image.value
-  }
-  return ''
-}
+export const rawImageLink = (src, config) => getImageUrl(src, {
+  preview: true,
+  ...config,
+})
 
 export default Image
---
+++ src/components/image.old.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import Img from 'gatsby-image'
+
+const Image = ({
+  image, className, style, alt = '',
+}) => {
+  if (!image) {
+    return null
+  }
+  if (image.childImageSharp) {
+    const { childImageSharp } = image
+    if (childImageSharp.fluid) {
+      return (
+        <Img
+          fluid={childImageSharp.fluid}
+          alt={alt}
+          className={className}
+          style={style}
+        />
+      )
+    }
+    if (childImageSharp.fixed) {
+      return (
+        <Img
+          fixed={childImageSharp.fixed}
+          alt={alt}
+          className={className}
+          style={style}
+        />
+      )
+    }
+    return null
+  }
+  if (typeof image === 'string') {
+    return <img src={image} className={className} style={style} alt={alt} />
+  }
+  if (image.value) {
+    return (
+      <img src={image.value} className={className} style={style} alt={alt} />
+    )
+  }
+  return null
+}
+
+export const getImageLink = (image) => {
+  if (!image) {
+    return ''
+  }
+  if (typeof image === 'string') {
+    return image
+  }
+  if (image.childImageSharp) {
+    const { childImageSharp } = image
+    if (childImageSharp.fluid) {
+      return childImageSharp.fluid.src
+    }
+    if (childImageSharp.fixed) {
+      return childImageSharp.fixed.src
+    }
+  }
+  if (image.value) {
+    return image.value
+  }
+  return ''
+}
+
+export default Image
