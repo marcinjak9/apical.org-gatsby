e0304d665e578a17fa57793a102dc6aeca818247
marcinjak9 <marcinjak9@gmail.com>
2019-04-30 11:09:41 +0200

    error wrapper

---
+++ src/components/ErrorBuondary.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  componentDidCatch(error, info) {
+    // Display fallback UI
+    this.setState({ hasError: true })
+    // You can also log the error to an error reporting service
+    console.error(error, info)
+  }
+
+  render() {
+    const { hasError } = this.state
+    const { children } = this.props
+    if (hasError) {
+      // You can render any custom fallback UI
+      return <h2>Something went wrong.</h2>
+    }
+    return children
+  }
+}
+
+export default ErrorBoundary
