b35c7d343ce7dc44a5be11b4a85bee23fcb397d0
marcinjak9 <marcinjak9@gmail.com>
2019-05-02 17:21:44 +0200

    refactor to external component

--- src/componentsMap.js
+++ src/componentsMap.js
@@ -1,23 +1,47 @@
-import HomeHero from './Wrappers/HomeHero'
-import WhatWeDo from './Wrappers/WhatWeDo'
-import TestimonialsSection from './Wrappers/TestimonialsSection'
-import BlogSection from './Wrappers/BlogSection'
-import HowItWorks from './Wrappers/HowItWorks'
-// import SectionContainer from './components/SectionContainer'
+// import HomeHero from './Wrappers/HomeHero'
+// import WhatWeDo from './Wrappers/WhatWeDo'
+// import TestimonialsSection from './Wrappers/TestimonialsSection'
+// import BlogSection from './Wrappers/BlogSection'
+// import HowItWorks from './Wrappers/HowItWorks'
+// // import SectionContainer from './components/SectionContainer'
+// import Presentation from './Wrappers/Presentation'
+// import Features from './Wrappers/Features'
+// import Cta from './Wrappers/Cta'
+// import SpecialFeatures from './Wrappers/SpecialFeatures'
+// import Quote from './Wrappers/Quote'
+// import WelcomeSteps from './Wrappers/WelcomeSteps'
+// import VideoSection from './Wrappers/VideoSection'
+// import FormSection from './Wrappers/FormSection'
+// import TeamSection from './Wrappers/TeamSection'
+// import SimpleHero from './Wrappers/SimpleHero'
+// import FeaturesTabs from './Wrappers/FeaturesTabs'
+// import ToolsCards from './Wrappers/ToolsCards'
+// import PricingSection from './Wrappers/PricingSection'
+// // import RestartingTypist from './components/TypistLoop'
+
+import {
+  HomeHero,
+  WhatWeDo,
+  TestimonialsSection,
+  BlogSection,
+  HowItWorks,
+  // Presentation,
+  Features,
+  Cta,
+  SpecialFeatures,
+  Quote,
+  WelcomeSteps,
+  VideoSection,
+  FormSection,
+  TeamSection,
+  SimpleHero,
+  FeaturesTabs,
+  ToolsCards,
+  // PrcingSection,
+} from 'apical-components'
+
 import Presentation from './Wrappers/Presentation'
-import Features from './Wrappers/Features'
-import Cta from './Wrappers/Cta'
-import SpecialFeatures from './Wrappers/SpecialFeatures'
-import Quote from './Wrappers/Quote'
-import WelcomeSteps from './Wrappers/WelcomeSteps'
-import VideoSection from './Wrappers/VideoSection'
-import FormSection from './Wrappers/FormSection'
-import TeamSection from './Wrappers/TeamSection'
-import SimpleHero from './Wrappers/SimpleHero'
-import FeaturesTabs from './Wrappers/FeaturesTabs'
-import ToolsCards from './Wrappers/ToolsCards'
 import PricingSection from './Wrappers/PricingSection'
-// import RestartingTypist from './components/TypistLoop'
 
 export default {
   HomeHero,
