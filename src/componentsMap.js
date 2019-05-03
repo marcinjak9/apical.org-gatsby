e0be239406d8477d6141eed75636c07906cc5b3c
marcinjak9 <marcinjak9@gmail.com>
2019-05-03 17:09:22 +0200

    added THE library

--- src/componentsMap.js
+++ src/componentsMap.js
@@ -1,26 +1,5 @@
-// import HomeHero from './Wrappers/HomeHero'
-// import WhatWeDo from './Wrappers/WhatWeDo'
-// import TestimonialsSection from './Wrappers/TestimonialsSection'
-// import BlogSection from './Wrappers/BlogSection'
-// import HowItWorks from './Wrappers/HowItWorks'
-// // import SectionContainer from './components/SectionContainer'
-// import Presentation from './Wrappers/Presentation'
-// import Features from './Wrappers/Features'
-// import Cta from './Wrappers/Cta'
-// import SpecialFeatures from './Wrappers/SpecialFeatures'
-// import Quote from './Wrappers/Quote'
-// import WelcomeSteps from './Wrappers/WelcomeSteps'
-// import VideoSection from './Wrappers/VideoSection'
-// import FormSection from './Wrappers/FormSection'
-// import TeamSection from './Wrappers/TeamSection'
-// import SimpleHero from './Wrappers/SimpleHero'
-// import FeaturesTabs from './Wrappers/FeaturesTabs'
-// import ToolsCards from './Wrappers/ToolsCards'
-// import PricingSection from './Wrappers/PricingSection'
-// // import RestartingTypist from './components/TypistLoop'
-
 import {
-  // HomeHero,
+  HomeHero,
   WhatWeDo,
   TestimonialsSection,
   BlogSection,
@@ -39,9 +18,6 @@ import {
   ToolsCards,
   PricingSection,
 } from 'apical-components'
-
-import HomeHero from './Wrappers/HomeHero'
-// import PricingSection from './Wrappers/PricingSection'
 
 export default {
   HomeHero,
