3661b85c41df337bad0be55c2c695e3301691ac4
marcinjak9 <marcinjak9@gmail.com>
2019-05-06 11:09:51 +0200

    added discover

--- src/Wrappers/DiscoverListing.js
+++ src/Wrappers/DiscoverListing.js
@@ -1,40 +1,54 @@
 import React from 'react'
-import Link from '../components/LinkWrapper'
-// import FilterTab from './FilterTab'
-
-// import CREATORS from '../data/creators.yaml'
-// import CreatorCardWithLightBox from './CreatorCardWithLightbox'
+import PropTypes from 'prop-types'
+import CreatorCardWithLightBox from './CreatorCardWithLightbox'
 import SectionContainer from '../components/SectionContainer'
 import { Row, Column } from '../components/Global'
 
-const DiscoverListing = () => (
-  <SectionContainer>
-    {/* <FilterTab /> */}
-    <Row>
-      {/* {CREATORS.creators
-        && CREATORS.creators.map((c, i) => (
-          <Column size="4">
-            <CreatorCardWithLightBox
-              image={c.image}
-              name={c.name}
-              body={c.body}
-              excerpt={c.excerpt}
-              tagline={c.tagline}
-              tag={c.tag}
-              url={c.url}
-              cta={{ text: 'More Festivals', url: c.url }}
-            />
-          </Column>
-        ))} */}
-    </Row>
-    <Row style={{ textAlign: 'center' }}>
-      <Column size="12" textCenter>
-        <Link to="/">
-          <a>View all</a>
-        </Link>
-      </Column>
-    </Row>
-  </SectionContainer>
-)
+const DiscoverListing = (props) => {
+  const { creators } = props
+  return (
+    <SectionContainer {...props}>
+      {/* <FilterTab /> */}
+      <Row>
+        {creators
+          && creators.length > 0
+          && creators.map((c, i) => (
+            <Column size="4">
+              <CreatorCardWithLightBox
+                image={c.image}
+                name={c.name}
+                body={c.body}
+                excerpt={c.excerpt}
+                tagline={c.tagline}
+                tag={c.tag}
+                url={c.url}
+              />
+            </Column>
+          ))}
+      </Row>
+      {/* <Row style={{ textAlign: 'center' }}>
+        <Column size='12' textCenter>
+          <Link to='/'>
+            <a>View all</a>
+          </Link>
+        </Column>
+      </Row> */}
+    </SectionContainer>
+  )
+}
+
+DiscoverListing.propTypes = {
+  creators: PropTypes.arrayOf(
+    PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string,
+      body: PropTypes.string,
+      excerpt: PropTypes.string,
+      tagline: PropTypes.string,
+      tag: PropTypes.string,
+      url: PropTypes.string,
+    }),
+  ),
+}
 
 export default DiscoverListing
--- src/componentsMap.js
+++ src/componentsMap.js
@@ -17,6 +17,8 @@ import SimpleHero from './Wrappers/SimpleHero'
 import FeaturesTabs from './Wrappers/FeaturesTabs'
 import ToolsCards from './Wrappers/ToolsCards'
 import PricingSection from './Wrappers/PricingSection'
+import DiscoverListing from './Wrappers/DiscoverListing'
+
 // import RestartingTypist from './components/TypistLoop'
 
 export default {
@@ -41,4 +43,5 @@ export default {
   FeaturesTabs,
   ToolsCards,
   PricingSection,
+  DiscoverListing,
 }
