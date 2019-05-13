baf44d0a390a778f00e1be3267b738a1548297b9
marcinjak9 <marcinjak9@gmail.com>
2019-05-13 13:36:00 +0200

    added ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,5 +112,13 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: 'UA-92828165-4',
+        debug: true,
+      },
+    },
   ],
 }
